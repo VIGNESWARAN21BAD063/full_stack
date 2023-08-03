957f47ef5283da00d1db7eaee15738e2e45ac7c6
Vicky <vickyvigneswaran007@gmail.com>
2023-08-03 16:42:35 +0530

    suma

---
+++ linkForHtml.js
@@ -0,0 +1,20 @@
+// document.write("Hello 3rd Years AI&DS Students")
+// window.alert("Don't open this page")
+// window.print()
+
+function first() {
+    document.getElementById("hello").innerHTML = "I am in my first year"
+}
+
+function second() {
+    document.getElementById("hello").innerHTML = "I am in my second year"
+}
+
+function third() {
+    document.getElementById("hello").innerHTML = "I am in my third year"
+}
+
+function final() {
+    document.getElementById("hello").innerHTML = "I am in my final year"
+}
+git
--- sample.js
+++ sample.js
@@ -45,4 +45,110 @@
 
 // array=[10,20]
 // array.forEach(element =>{console.log(element)});
-                                                                                          
+
+
+
+//Array
+
+
+// const array = [10,20,"PW",23.08 , Date() ]
+// console.log("Before change:")
+// for (var ele of array) {
+//     console.log(ele)
+// }
+
+// after
+// console.log("After change:")
+// array.push("3rd year")
+// for (var ele of array) {
+//     console.log(ele)
+// }
+
+//  pop
+// array.pop()
+// console.log("After pop:")
+
+// for (var ele of array) {
+//     console.log(ele)
+// }
+
+// array.pop()
+// array.pop()
+// array.pop()
+// array.pop()
+// array.pop()
+// if (array.length > 0) {
+//     console.log ("Elements are in the array")
+//     for (var ele of array) {
+//         console.log(ele)
+//     }
+// }
+// else {
+//     console.log ("No elements are in the array")
+// }
+
+// array.push(1)
+// if (array.length > 0) {
+//     console.log ("Elements are in the array")
+//     for (var ele of array) {
+//         console.log(ele)
+//     }
+// }
+// else {
+//     console.log ("No elements are in the array")
+// }
+
+
+// // Objects
+// b = {
+//     car : "Audi",
+//     model : "AB",
+//     airbags : 8,
+//     wheels : 4,
+//     color : "Blue"
+// }
+
+//  accessing the object
+// console.log(b["color"])
+// console.log(b.airbags)
+// var some = "model"
+// console.log(b[some])
+// console.log(b)
+
+// changing the value of an object
+// b.car = "BMW"
+// console.log(b)
+
+// /adding new value and nested object
+// b.sunroof = {
+//     is_present : "Yes",
+//     Shape : "Rectangle"
+// }
+// console.log(b)
+// // accessing nested object
+// console.log(b.sunroof.is_present)
+// for (var ele in b) {
+//     console.log(ele + ":" + b[ele] )
+//     if (typeof b[ele] == "object")
+//     {
+//         for (var val in b[ele]) {
+//             console.log (val + ":" + b[ele][val])
+//         }
+//     }
+// }
+
+//Functions
+
+// function fun_name(){
+//     console.log(5+5)
+//     console.log("PSN"+5+5)
+// }
+//  fun_name()
+
+//  function f_name(a,b){
+//     console.log(a+b)
+//     console.log(a+b+5+8+'Vicky'+5+5)
+//  }
+//  f_name(5,5)
+
+
