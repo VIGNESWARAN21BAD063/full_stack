990699fb54e36dc665079d3582fe946d4c7edd71
Vicky <vickyvigneswaran007@gmail.com>
2023-08-03 12:20:48 +0530

    suma

---
+++ sample.js
@@ -0,0 +1,31 @@
+// var a=120;
+// // console.log("This is tha value declared in var type:"+" "+ a)
+
+// a=0
+// if(a===0){
+//     console.log("The number is 0 and adding 1 making it positive")
+// }
+
+ 
+//for loop:
+
+
+// a=5
+// for(i=0;i<a;i++)
+// {
+//   console.log(i)
+// }
+
+//While loop:
+
+// i=0
+// while(i<=a){
+//     console.log(i)
+//     i++
+// }
+
+//for each:
+
+// array=[10,20]
+// array.forEach(element =>{console.log(element)});
+                                                                                          
